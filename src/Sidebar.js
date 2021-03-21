abdc26ee7373d6e8cf298c1fc7bcd73337225000
DEEPTEJ RANE <deeptej@DEEPTEJs-MacBook-Pro.local>
2021-03-22 01:44:22 +0530

    changed sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 //Get material-ui icons
-import TwitterIcon from '@material-ui/icons/Twitter';
 import SidebarOptions from './SidebarOptions'
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
@@ -16,7 +15,6 @@ import './Sidebar.css'
 function Sidebar() {
     return (
         <div className="sidebar">
-            <TwitterIcon className="sidebar__twitterIcon"/>
             {/*pass icons as props to SidebarOptions component*/}
             {/*keep i captial of Icon to let react know you are passing a component*/}
             <SidebarOptions active text="Home" Icon={HomeIcon}/>
@@ -27,7 +25,7 @@ function Sidebar() {
             <SidebarOptions text="Lists" Icon={ListAltIcon}/>
             <SidebarOptions text="Profile" Icon={PermIdentityIcon}/>
             <SidebarOptions text="More" Icon={MoreHorizIcon}/>
-            <Button variant="outlined"  className="sidebar__tweet" fullWidth>Tweet</Button>
+            <Button variant="outlined"  className="sidebar__tweet" fullWidth>Texx</Button>
         </div>
     )
 }
--- src/Widgets.js
+++ src/Widgets.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Widgets.css'
 import { Input } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-// import {TwitterTweetEmbed, TwitterTimelineEmbed, TwitterShareButton} from 'react-twitter-embed'
+
 
 function Widgets() {
     return (
@@ -12,10 +12,7 @@ function Widgets() {
                 <Input style={{color:"aliceblue"}} className="widgets__input" type= "text" placeholder="Search Texx" />
             </div>
             <div className="widgets___widgetContainer">
-               {/* <h2> What's happening...</h2>
-                <TwitterTweetEmbed tweetId={'1341399855856558086'}/>
-                <TwitterTimelineEmbed sourceType="profile" screeName="murtikara" options={{height:400}}/>
-                <TwitterShareButton url={'https://twitter.com/murtikara2'} options={{text:"React is love"}}/> */}
+
             </div>
         </div>
     )

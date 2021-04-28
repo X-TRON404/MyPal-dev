0a7adbd42559fd13157cdc8c79972ea63f0bfeac
DEEPTEJ RANE <deeptej@DEEPTEJs-MacBook-Pro.local>
2021-04-28 20:29:10 +0530

    Added <Profile> component

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -15,6 +15,8 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import './Profile.css'
+import { Button } from '@material-ui/core';
 
 
 
@@ -35,9 +37,6 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-  },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
@@ -52,9 +51,14 @@ const useStyles = makeStyles((theme) => ({
   expandOpen: {
     transform: 'rotate(180deg)',
   },
-  avatar: {
-    backgroundColor: red[500],
-  },
+  avatar:{
+        width: theme.spacing(15),
+        height: theme.spacing(15),
+        marginTop:10,
+        marginLeft:10,
+        marginBottom:10,
+        marginRight:10,
+      },
 }));
 
 function Profile() {
@@ -66,78 +70,53 @@ function Profile() {
   };
 
     return (
-        <div>
-                <Card className={classes.root}>
-                    <CardHeader
-                        avatar={
-                        <Avatar aria-label="recipe" className={classes.avatar}>
-                            R
-                        </Avatar>
-                        }
-                        action={
-                        <IconButton aria-label="settings">
-                            <MoreVertIcon />
-                        </IconButton>
-                        }
-                        title="Shrimp and Chorizo Paella"
-                        subheader="September 14, 2016"
-                    />
-                    <CardMedia
-                        className={classes.media}
-                        image="/static/images/cards/paella.jpg"
-                        title="Paella dish"
-                    />
+        <div className="profile">
+                <Card className="profile__card" elevation={8}>
+                    <div className="profile__header">
+                        <div className="profile__headerTop">
+                                <Avatar className={classes.avatar} alt={'username'} src="/static/images/avatar/1.jpg" ></Avatar>
+                                <div className="profle__headerInfo">
+                                    <Typography>{'username'}</Typography>
+                                    <Typography>{`Joined on " + ${'Joining date'}`}</Typography>
+                                </div>
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                            
+                        </div>
+                        <center className="profile__headerEditButton"><Button >Edit Profile</Button></center>
+                        <div className="profile__headerTypography">
+                            <Typography>Biography</Typography>
+                            <Typography>Interests</Typography>
+                        </div>
+                    </div>
+                    <div className="profile__footer">
                     <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                        This impressive paella is a perfect party dish and a fun meal to cook together with your
-                        guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                        <Typography variant="h5">
+                            STATS
                         </Typography>
+                        <div className="profile__footerStats">
+                            <div className="profile__footerStatsPosts">
+                                <Typography variant="body2" >
+                                    POSTS
+                                </Typography>
+                                <p>{'no. of posts'}</p>
+                            </div>
+                            <div className="profile__footerStatsEvents">
+                                <Typography variant="body2" >
+                                    EVENTS
+                                </Typography>
+                                <p>{'no. of events took part in '}</p>
+                            </div>
+                            <div className="profile__footerStatsFriends">
+                                <Typography variant="body2" >
+                                    NEW FRIENDS
+                                </Typography>
+                                <p>{'no. of friends'}</p>
+                            </div>
+                        </div>
                     </CardContent>
-                    <CardActions disableSpacing>
-                        <IconButton aria-label="add to favorites">
-                        <FavoriteIcon />
-                        </IconButton>
-                        <IconButton aria-label="share">
-                        <ShareIcon />
-                        </IconButton>
-                        <IconButton
-                        className={clsx(classes.expand, {
-                            [classes.expandOpen]: expanded,
-                        })}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label="show more"
-                        >
-                        <ExpandMoreIcon />
-                        </IconButton>
-                    </CardActions>
-                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                        <CardContent>
-                        <Typography paragraph>Method:</Typography>
-                        <Typography paragraph>
-                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                            minutes.
-                        </Typography>
-                        <Typography paragraph>
-                            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                        </Typography>
-                        <Typography paragraph>
-                            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                            minutes more. (Discard any mussels that don’t open.)
-                        </Typography>
-                        <Typography>
-                            Set aside off of the heat to let rest for 10 minutes, and then serve.
-                        </Typography>
-                        </CardContent>
-                    </Collapse>
+                    </div>
                     </Card>
         </div>
     )

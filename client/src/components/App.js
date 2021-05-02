7fbaca1c171b073ded14c027e92682b478019b02
DEEPTEJ RANE <deeptej@DEEPTEJs-MacBook-Pro.local>
2021-05-02 12:18:25 +0530

    Link to home from texx logo working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -252,7 +252,7 @@ auth.signOut().then(() => {
           <div className="app__header">
               {/* <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" alt="ig-logo"/> */}
               <Router>
-                <Link to="/"><h1>Texx</h1></Link>
+                <Link to="/" onClick={()=>window.location.href= '/'}><h1>Texx</h1></Link>
               </Router>
 
                                                 {/*sign up/sign in or log out the user*/}

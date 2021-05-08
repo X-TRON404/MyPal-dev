99858258ca35a9e0986da7dfb47c7e0865b85327
DEEPTEJ RANE <deeptej@DEEPTEJs-MacBook-Pro.local>
2021-05-08 14:32:15 +0530

    fixed the typo for title prop

--- client/src/components/FeedEvents.js
+++ client/src/components/FeedEvents.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Event from './Event';
 import {DataBase} from './firebase'
 import {useStateValue} from '../contexts/StateProvider'
+import './FeedEvents.css'
 
 function FeedEvents() {
 
@@ -16,15 +17,20 @@ const [events, setEvents] = useState([]);
         //grab the collection 'events' from the database and order 'docs' in the collection by timestamp
         DataBase.collection('events').orderBy('timestamp','desc').onSnapshot(snapshot =>{
         //Now set the  id=doc.id and event=doc.data to the fields in the 'event' variable that we defined above
+        console.log(snapshot.docs.map(doc =>({id:doc.id,event:doc.data()})))
         setEvents(snapshot.docs.map(doc =>({id:doc.id,event:doc.data()})))
             })
+    
+        return 
+
+    
     },[user]);
     return (
     <div className="feedEvents">
             <div className="feedEvents__events"> 
                  {
                 //render only those posts by id who are newly added to the database dont render the entire post list  
-                posts.map(({id,event})=>(<Event key={event.id} postId={id} date={event.date} time={event.time} venue={event.venue} title={event.titile} username={event.username} user_id={event.user_id} description={event.description} imageUrl={event.imageUrl} interestedCount={event.interestedCount}/>))
+                events.map(({id,event})=>(<Event key={event.id} eventId={id} dateTime={event.dateTime} venue={event.venue} title={event.title} username={event.username} user_id={event.user_id} description={event.description} imageUrl={event.imageUrl} interestedCount={event.interestedCount}/>))
                 } 
             </div>    
     </div>

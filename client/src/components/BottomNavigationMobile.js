869c6c17006af30c2a698f1abfc232b5b2e9aed7
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-13 23:24:19 +0530

    Added feature for active button label

--- client/src/components/BottomNavigationMobile.js
+++ client/src/components/BottomNavigationMobile.js
@@ -18,23 +18,31 @@ const useStyles = makeStyles({
   },
 });
 
+const navValues = {
+  '/':1,
+  '/confessionsFeed':2,
+  '/eventsFeed':3,
+  '/chatsFeed':4,
+  '/notificationsFeed':5
+}
+
+//BUG:
+// here everytime we change the value using setValue it causes one additional render
 function BottomNavigationMobile() {
     const classes = useStyles();
-    const [value, setValue] = useState('Home');
-  
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
+    const pathname = window.location.pathname; // in case user visits the path directly. The BottomNavBar is able to follow suit.
+    const [value, setValue] = useState(navValues[window.location.pathname])
     return (
       <Paper elevation={10}>
         <div className="bottomNavigation"> 
-            <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+            <BottomNavigation value={value} className={classes.root}>
+              {console.log(value)}
             {/*Added routers here so that instead of pushing the entire app.js to rerender when we click it will only re-render the specific component inside the app__body <Router>*/}
-            <BrowserRouter><Link><BottomNavigationAction onClick={()=>window.location.href= '/'} label="Home" value="Home" icon={<HomeIcon/>} /></Link></BrowserRouter>
-            <BrowserRouter><Link><BottomNavigationAction onClick={()=>window.location.href= '/confessionsFeed'} label="Confessions" value="Confessions" icon={<WhatshotSharpIcon/>} /></Link></BrowserRouter>
-            <BrowserRouter><Link><BottomNavigationAction onClick={()=>window.location.href= '/eventsFeed'} label="Event" value="Events" icon={<EventIcon/>} /></Link></BrowserRouter>
-            <BrowserRouter><Link><BottomNavigationAction onClick={()=>window.location.href= '/chatsFeed'} label="Chats" value="Chats" icon={<CommentRoundedIcon />} /></Link></BrowserRouter>
-            <BrowserRouter><Link><BottomNavigationAction onClick={()=>window.location.href= '/'} label="Notifications" value="Notifications" icon={ <NotificationsNoneIcon/>} /></Link></BrowserRouter>
+            <BottomNavigationAction onClick={()=>{window.location.href= '/';setValue(navValues[window.location.pathname])}} label="Home"  icon={<HomeIcon/>} />
+            <BottomNavigationAction onClick={()=>{window.location.href= '/confessionsFeed';setValue(navValues[window.location.pathname])}} label="Confessions" icon={<WhatshotSharpIcon/>} />
+            <BottomNavigationAction onClick={()=>window.location.href= '/eventsFeed'} label="Events"  icon={<EventIcon/>} />
+            <BottomNavigationAction onClick={()=>window.location.href= '/chatsFeed'} label="Chats"  icon={<CommentRoundedIcon />} />
+            <BottomNavigationAction onClick={()=>window.location.href= '/notificationsFeed'} label="Notifications"  icon={ <NotificationsNoneIcon/>} />
             </BottomNavigation>
         </div>
       </Paper>

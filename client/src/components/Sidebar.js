2eb215c49a2992908467809de054b8aa95ab7edf
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-17 23:50:54 +0530

    removed window.loc because this comp is now a child of <Router>

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -18,26 +18,16 @@ function Sidebar() {
         <div className="sidebar">
             {/*pass icons as props to SidebarOptions component*/}
             {/*keep i captial of Icon to let react know you are passing a component*/}
-            <Router>
-                <Link to='/' onClick={()=>window.location.href= '/'}><SidebarOptions active text="Home" Icon={HomeIcon}/></Link>
-            </Router>
+            <Link to='/'><SidebarOptions active text="Home" Icon={HomeIcon}/></Link>
             <SidebarOptions text="Explore" Icon={SearchIcon}/>
-            <Router>
-                <Link to='/eventsFeed' onClick={()=>window.location.href='/eventsFeed'}><SidebarOptions text="Events" Icon={EventIcon}/></Link>
-            </Router>
-            <Router>
-                <Link to='/confessionsFeed' onClick={()=>window.location.href='/confessionsFeed'}><SidebarOptions text="Confessions" Icon={WhatshotSharpIcon}/></Link>
-            </Router>
+            <Link to='/eventsFeed'><SidebarOptions text="Events" Icon={EventIcon}/></Link>
+            <Link to='/confessionsFeed' onClick={()=>window.location.href='/confessionsFeed'}><SidebarOptions text="Confessions" Icon={WhatshotSharpIcon}/></Link>
             <SidebarOptions text="Notifications" Icon={NotificationsNoneIcon}/>
             <SidebarOptions text="Messages" Icon={MailOutlineIcon}/>
             <SidebarOptions text="Bookmarks" Icon={BookmarkBorderIcon}/>
             <SidebarOptions text="More" Icon={MoreHorizIcon}/>
-            <Router>
-                <Link to='/createEvent' onClick={()=>window.location.href='/createEvent'}><Button variant="outlined"  className="sidebar__event" fullWidth>New event</Button></Link>
-            </Router>
-            <Router>
-                <Link to='/createConfessions' onClick={()=>window.location.href='/createConfessions'}><Button variant="outlined"  className="sidebar__event" fullWidth>Write an annonymous confession</Button></Link>
-            </Router>
+            <Link to='/createEvent' onClick={()=>window.location.href='/createEvent'}><Button variant="outlined"  className="sidebar__event" fullWidth>New event</Button></Link>
+            <Link to='/createConfessions' onClick={()=>window.location.href='/createConfessions'}><Button variant="outlined"  className="sidebar__event" fullWidth>Write an annonymous confession</Button></Link>
         </div>
     )
 }

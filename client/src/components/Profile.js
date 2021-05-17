dc0ac87c6f4740eb1e2bcf87132e659aaabedd16
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-17 23:49:45 +0530

    Added a conditional loading for bio

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,23 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import './Profile.css'
 import { Button, Input } from '@material-ui/core';
-import {useStateValue} from '../contexts/StateProvider'
 import { auth, DataBase } from './firebase';
 import firebase from 'firebase/app'
 
@@ -75,7 +65,7 @@ useEffect(() => {
 //displayName to edit the existing username
 const [displayName,setDisplayName] = useState(user?.displayName);
 // edit bio
-const [bio,setBio] = useState(userInfo.bio);
+const [bio,setBio] = useState(userInfo?.bio);
 console.log(bio)
 //==================================================Log out =======================================
 const logout = () => {
@@ -105,7 +95,7 @@ const logout = () => {
           });
         }
         //if bio is changed
-        else if (bio !== userInfo.bio){
+        else if (bio !== userInfo?.bio){
             DataBase.collection('users').doc(user.uid).set({
                 bio:bio
             })

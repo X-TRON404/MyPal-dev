36d9152762927a50d3199415017f51bafe1d4f63
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-17 23:35:58 +0530

    Added <BottomNav> as children of Router hence removed window.location.href

--- client/src/components/BottomNavigationMobile.js
+++ client/src/components/BottomNavigationMobile.js
@@ -3,13 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import EventIcon from '@material-ui/icons/Event';
-import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
 import CommentRoundedIcon from '@material-ui/icons/CommentRounded';
 import HomeIcon from '@material-ui/icons/Home';
 import './BottomNavigationMobile.css'
 import WhatshotSharpIcon from '@material-ui/icons/WhatshotSharp';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
-import { Card, Paper } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 import {BrowserRouter, Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
@@ -30,19 +29,19 @@ const navValues = {
 // here everytime we change the value using setValue it causes one additional render
 function BottomNavigationMobile() {
     const classes = useStyles();
-    const pathname = window.location.pathname; // in case user visits the path directly. The BottomNavBar is able to follow suit.
-    const [value, setValue] = useState(navValues[window.location.pathname])
+    const [value, setValue] = React.useState('Home');
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
     return (
       <Paper elevation={10}>
         <div className="bottomNavigation"> 
-            <BottomNavigation value={value} className={classes.root}>
-              {console.log(value)}
-            {/*Added routers here so that instead of pushing the entire app.js to rerender when we click it will only re-render the specific component inside the app__body <Router>*/}
-            <BottomNavigationAction onClick={()=>{window.location.href= '/';setValue(navValues[window.location.pathname])}} label="Home"  icon={<HomeIcon/>} />
-            <BottomNavigationAction onClick={()=>{window.location.href= '/confessionsFeed';setValue(navValues[window.location.pathname])}} label="Confessions" icon={<WhatshotSharpIcon/>} />
-            <BottomNavigationAction onClick={()=>window.location.href= '/eventsFeed'} label="Events"  icon={<EventIcon/>} />
-            <BottomNavigationAction onClick={()=>window.location.href= '/chatsFeed'} label="Chats"  icon={<CommentRoundedIcon />} />
-            <BottomNavigationAction onClick={()=>window.location.href= '/notificationsFeed'} label="Notifications"  icon={ <NotificationsNoneIcon/>} />
+            <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+            <BottomNavigationAction component={Link} to ='/' value="Home"  label="Home"  icon={<HomeIcon/>} />
+            <BottomNavigationAction component={Link} to= '/confessionsFeed' value="Confessions" label="Confessions" icon={<WhatshotSharpIcon/>} />
+            <BottomNavigationAction component={Link} to='/eventsFeed' value="Events" label="Events"  icon={<EventIcon/>} />
+            <BottomNavigationAction component={Link} to= '/chatsFeed' value="Chats"  label="Chats"  icon={<CommentRoundedIcon />} />
+            <BottomNavigationAction component={Link} to= '/notificationsFeed' value="Notifications" label="Notifications"  icon={ <NotificationsNoneIcon/>} />
             </BottomNavigation>
         </div>
       </Paper>

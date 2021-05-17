33a1d49e0b4ff4fcf51bf9871151e1ab2c8fc8c9
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-17 23:47:25 +0530

    made widgetsChat a child of Router hence removed window.loc.href and also removed dispatch as we dont need that anymore

--- client/src/components/chat/WidgetsChat.js
+++ client/src/components/chat/WidgetsChat.js
@@ -4,18 +4,15 @@
 import React, { useEffect, useState } from 'react'
 import './WidgetsChat.css'
 import Badge from '@material-ui/core/Badge';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import { makeStyles} from '@material-ui/core/styles';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import {ButtonBase} from '@material-ui/core';
 import {BrowserRouter, Link} from 'react-router-dom';
 import {DataBase} from '../firebase'
-import firebase from 'firebase/app';
 import {useStateValue} from '../../contexts/StateProvider';
 import { actionTypes } from '../../contexts/reducer';
-import { Unsubscribe } from '@material-ui/icons';
 //==================================================Card Styles==============================================
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -65,11 +62,9 @@ function WidgetsChat() {
         //if user is logged out it throws an error hence using try catch
         try {
             console.log("trying widgetsChat")
-            
-            console.log(user)
-            //this is not working because the user that is being added to 'users' collection from the App.js signup is not creating a valid document
             DataBase.collection('users').doc(user.uid).collection('chats').onSnapshot((snapshot)=>(
                 setChats(snapshot.docs.map((doc) => doc.data()))
+                
                
             ))
             console.log(chats)
@@ -88,17 +83,7 @@ function WidgetsChat() {
                  {/* <div className="widgetsChat__onlineMembers"> */}
             {
                 chats.map((chat)=>(
-                <BrowserRouter>
-                            <Link to={`/chats/${chat.chat_user_id}`} key={chat.chat_user_id} onClick={   
-                                ()=>{ //dispatch the chat_user_id when the user clicks on the Link, to the global state
-                                      //later used in App.js to render <SendMessage> and pass 'chatId' to SendMessage
-                                          dispatch(
-                                                        {
-                                                        type:actionTypes.SET_CHAT_INPUT,
-                                                        chatId:chat.chat_user_id,
-                                                        chatInput:true,
-                                                        
-                                                        }); window.location.href= `/chats/${chat.chat_user_id}` }}>
+                    <Link to={`/chats/${chat.chat_user_id}`} key={chat.chat_user_id}>
                             <div className="widgetsChat__onlineBox">
                                 <ButtonBase>
                                                 <Avatar className={classes.avatar} alt={'d'} src="/static/images/avatar/1.jpg"></Avatar>
@@ -122,8 +107,7 @@ function WidgetsChat() {
                                                 </div>
                                 </ButtonBase>
                             </div>
-                            </Link>
-                </BrowserRouter>
+                    </Link>
                 
                 ))
             }

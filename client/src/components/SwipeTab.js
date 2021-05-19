d83a95679c67d47781b0de0cb1f6420c29e5959b
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-20 01:50:50 +0530

    Added <YourEvents>

--- client/src/components/SwipeTab.js
+++ client/src/components/SwipeTab.js
@@ -7,6 +7,7 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import YourPosts from './YourPosts'
+import YourEvents from './YourEvents'
 import './SwipeTab.css'
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -78,7 +79,7 @@ export default function ScrollableTabsButtonAuto() {
         <YourPosts/>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item Two
+        <YourEvents/>
       </TabPanel>
       <TabPanel value={value} index={2}>
         Item Three

85f6c69e764b057e1e7d2c9008ffdf21d7c743b0
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-19 17:04:07 +0530

    added cloud function to get the status of the user from realtime db and removed unnecessary files that were present earlier

---
+++ server/functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const Firestore = require('@google-cloud/firestore');
+
+const firestore = new Firestore();
+
+exports.onUserStatusChanged = functions.database
+  .ref('/status/{userId}') // Reference to the Firebase RealTime database key
+  .onUpdate(event => {
+    const usersRef = firestore.collection('/users'); // Create a reference to the Firestore Collection
+  
+    return event.data.ref.once('value')
+      .then(statusSnapshot => snapShot.val()) // Get the latest value from the Firebase Realtime database
+      .then(status => {
+        // check if the value is 'offline'
+        if (status === 'offline') {
+          // Set the Firestore's document's online value to false
+          usersRef
+            .doc(event.params.userId)
+            .set({
+              online: false
+            }, { merge: true });
+        }
+      })
+  });

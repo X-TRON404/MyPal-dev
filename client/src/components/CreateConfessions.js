676796d4af8035db80ff68aaf4791d251b9b6879
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-21 23:39:44 +0530

    added alert component once the confession is done uploading

--- client/src/components/CreateConfessions.js
+++ client/src/components/CreateConfessions.js
@@ -4,12 +4,15 @@ import firebase from 'firebase';
 import {Button,TextField} from '@material-ui/core';
 import {useStateValue} from '../contexts/StateProvider'
 import './CreateConfessions.css'
+import AlertDialog from './AlertDialog';
 
 function CreateConfessions() {
     //get the user from the provider
     const [{user}, dispatch] = useStateValue();
     //store confession
     const [confession,setConfession] = useState('')
+    //open alert box when a new event is created
+    const [openAlert,setOpenAlert] = useState(false)
 
     const handlePost = (e) => {
         DataBase.collection('confessions').add({
@@ -18,13 +21,15 @@ function CreateConfessions() {
             timestamp:firebase.firestore.FieldValue.serverTimestamp(),
         })
         setConfession('')
+        setOpenAlert(true)
 
     }
     return (
         <div className="createConfessions">
+            <AlertDialog text={"Your new confession is up!"} openAlert={openAlert} changeAlert={al=>{setOpenAlert(al)}}/>
                 <form className="createConfessions__form">
                     <h2 id="createConfessions_h2"> Write a Confession </h2>
-                    <TextField placeholder = "Write your heart out..." className = "createConfessions__textarea" onChange={(e)=>setConfession(e.target.value)} value={confession}
+                    <TextField className = "createConfessions__textarea" onChange={(e)=>setConfession(e.target.value)} value={confession}
                                     id="outlined-multiline-static"
                                     label="Write a confession..."
                                     multiline

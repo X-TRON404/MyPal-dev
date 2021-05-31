80dba2a1e87242f33974509ecbe669fadad7216d
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-05-31 23:21:09 +0530

    refactor:splitted the component into its child <WidgetMember>

--- client/src/components/chat/WidgetsChat.js
+++ client/src/components/chat/WidgetsChat.js
@@ -1,18 +1,14 @@
 
 //users in sidebar chat (online/offline)
 
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import './WidgetsChat.css'
-import Badge from '@material-ui/core/Badge';
 import { makeStyles} from '@material-ui/core/styles';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
-import {ButtonBase} from '@material-ui/core';
-import {BrowserRouter, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {DataBase} from '../firebase'
 import {useStateValue} from '../../contexts/StateProvider';
-import { actionTypes } from '../../contexts/reducer';
+import Skeleton from '@material-ui/lab/Skeleton';
+const WidgetMember = React.lazy(() => import('./WidgetMember'))
 //==================================================Card Styles==============================================
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -54,85 +50,48 @@ function WidgetsChat() {
     //get the user from the provider  
     const [{user}, dispatch] = useStateValue();
     const classes = useStyles();
-    const [messages, setMessages] = useState([]);
     const [chats, setChats] = useState([])
     
+
 
     useEffect(() => {
         //if user is logged out it throws an error hence using try catch
         try {
-            console.log("trying widgetsChat")
-            DataBase.collection('users').doc(user.uid).collection('chats').onSnapshot((snapshot)=>(
-                setChats(snapshot.docs.map((doc) => doc.data()))
+        
+                
+               DataBase.collection('users').doc(user.uid).collection('chats').onSnapshot((snapshot)=>(
+                    setChats(snapshot.docs.map((doc) => doc.data()))
+                ))
+                console.log(chats)
                 
                
-            ))
-            console.log(chats)
-     }
-        catch (error) {
-        console.log(error.message+" coming from widgetsChat getting 'chats'collection ")
+           
         }
-        
-    }, [user]);
+        catch (error) {
+            console.log(error.message+" coming from widgetsChat getting 'chats'collection ")
+            }
+    }, [,user]);
+    console.log(chats)
+
 
     return (
-//==========================================Online members===========================================================
+//==========================================ALL CHATS LIST===========================================================
         <div className="widgetsChat">
-            <div className="widgetsChat__online">
-                 <p className="widgetsChat__headerTitle">Online<span><Badge color="primary" overlap="circle"  variant="dot"></Badge></span></p>
-                 {/* <div className="widgetsChat__onlineMembers"> */}
-            {
-                chats.map((chat)=>(
-                    <Link to={`/chats/${chat.chat_user_id}`} key={chat.chat_user_id}>
-                            <div className="widgetsChat__onlineBox">
-                                <ButtonBase>
-                                                <Avatar className={classes.avatar} alt={'d'} src="/static/images/avatar/1.jpg"></Avatar>
-                                                    <Badge
-                                                        overlap="circle"
-                                                        anchorOrigin={{
-                                                        vertical: 'bottom',
-                                                        horizontal: 'right',
-                                                        }}
-                                                       
-                                                    ></Badge>
-                                                <div className={classes.details}>
-                                                        <CardContent className={classes.content}>
-                                                            <Typography component="p" variant="p">
-                                                                {chat.chat_username}
-                                                            </Typography>
-                                                            <Typography className={classes.typoStatus} variant="caption">
-                                                                status like I am using texx
-                                                            </Typography>
-                                                        </CardContent>
-                                                </div>
-                                </ButtonBase>
-                            </div>
-                    </Link>
-                
+          CHATS
+          {chats.length!==0 &&  
+                (chats.map((chat)=>(
+                    <Suspense fallback={
+                        <div><Skeleton variant="text" />
+                        <Skeleton variant="circle" width={40} height={40} />
+                        <Skeleton variant="rect" width={210} height={118} /></div>} key={chat.chat_user_id}>
+                        <Link to={`/chats/${chat.chat_user_id}`} >
+                          <WidgetMember chatId={chat.chat_user_id} chat_username= {chat.chat_username} />
+                        </Link>
+                        {console.log(chat.chat_user_id)}
+                    </Suspense>
                 ))
-            }
-                 
-                 </div>
-            {/* </div> */}
-                                                        {/* offline members */}
-            <div className="widgetsChat__offline">
-                 <p className="widgetsChat__headerTitle">Offline<span><Badge color="secondary" overlap="circle"  variant="dot"></Badge></span></p> 
-                 <div className="widgetsChat__offlineMembers">
-                 {/* <div className="widgetsChat__onlineBox">
-                            <Avatar className={classes.avatar} alt={'d'} src="/static/images/avatar/1.jpg"></Avatar>
-                            <div className={classes.details}>
-                                    <CardContent className={classes.content}>
-                                        <Typography component="p" variant="p">
-                                            User
-                                        </Typography>
-                                        <Typography className={classes.typoStatus} variant="caption">
-                                            status
-                                        </Typography>
-                                    </CardContent>
-                            </div>
-                     </div> */}
-                 </div>
-            </div>
+            )
+            }            
         </div>
     )
 }

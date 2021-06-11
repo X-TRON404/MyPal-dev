93c67b76752fe7d57f52ba6ade8ce2149d87f9a7
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-06-12 00:34:50 +0530

    refactor:removed <Suspender><Skeleton> elements which were not matching with the <Event> structure

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -90,9 +90,7 @@ useEffect(() => {
                     </span>
                 </div>
                 <Suspense fallback={
-                        <div><Skeleton variant="text" />
-                        <Skeleton variant="circle" width={40} height={40} />
-                        <Skeleton variant="rect" width={210} height={118} /></div>} >
+                        <div><Skeleton variant="text" /></div>} >
                             <EventsMenu eventId={eventId} eventUsername={username} eventUserId={user_id}/>
                 </Suspense>
             </div>

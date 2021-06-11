72e0638c8d9a2715b0872f269b1338d1548414e6
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-06-12 00:26:03 +0530

    refactor:New messages will now be sent to Realtime db instead of Firestore;because of change in messaging db from Firestore to Realtime

--- client/src/components/chat/SendMessage.js
+++ client/src/components/chat/SendMessage.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { Button, IconButton, Input } from '@material-ui/core';
+import {IconButton, Input } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import firebase from 'firebase/app'
 import {useStateValue} from '../../contexts/StateProvider';
 import {InsertEmoticon, MicOutlined} from '@material-ui/icons';
-import {DataBase} from '../firebase';
+import {DataBase, realtime} from '../firebase';
 import './SendMessage.css'
 
 
@@ -16,42 +16,58 @@ function SendMessage({chatId}) {
   
 //========================================================POST Messages========================================
 
+const sendMessage = () => {
+    if (user){
+                   //add message to user1's database (sender)
+                   realtime
+                   .ref(`/'messages'/${user.uid}/${chatId}`)
+                   //push create a unique id for each new doc 
+                   .push({
+                       text:input,
+                       timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+                       author:user.uid,
+                       authorName:user.displayName,
+                       imageUrl:"",},
+                       (error) => {
+                       if (error) {
+                       alert(error.message)
+                       } else {
+                       //successful!!
+                       }         
+                   })
+                   //add message to user2's database (sendee)
+                   //add to realtime db 
+                       realtime
+                       .ref(`/'messages'/${chatId}/${user.uid}`)
+                       //push create a unique id for each new doc 
+                       .push({     
+                               text:input,
+                               timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+                               author:user.uid,
+                               authorName:user.displayName,
+                               imageUrl:"",
+                           },
+                           (error) => {
+                           if (error) {
+                           alert(error.message)
+                           } else {
+                           //successful!!
+                           }
+                       
+                       })
 
-    const sendMessage = () => {
-        if (user){
-            //add message to user1's database (sender)
-            DataBase.collection('users').doc(user.uid).collection('chats').doc(chatId).collection('messages').add(
-                {
-                    text:input,
-                    timestamp:firebase.firestore.FieldValue.serverTimestamp(),
-                    author:user.uid,
-                    authorName:user.displayName,
-                    imageUrl:"",
-                }
-            )
-            //add message to user2's database (sendee)
-            DataBase.collection('users').doc(chatId).collection('chats').doc(user.uid).collection('messages').add(
-                {
-                    text:input,
-                    timestamp:firebase.firestore.FieldValue.serverTimestamp(),
-                    author:user.uid,
-                    authorName:user.displayName,
-                    imageUrl:"",
-                }
-            )
-
-            setInput('');
-        }
+        setInput('');
     }
+}
 
 //=============================================================================================================
     return (
     <div className="sendMessage">
             <form className="chat__inputForm" onSubmit={(e)=>{e.preventDefault()}}>
-                <InsertEmoticon/>
+                {/* <InsertEmoticon/> */}
                 <Input style={{color:"aliceblue"}} className="sendMessage__input" value={input} onChange={(e)=>setInput(e.target.value)} type="text" placeholder="   Send a texx..."/>
                 <IconButton  variant ='contained' color="primary"disabled={!input} onClick={sendMessage} type="submit"><SendIcon /></IconButton>
-                <MicOutlined/>
+                {/* <MicOutlined/> */}
             </form>
     </div>
     )

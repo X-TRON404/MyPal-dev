c23c1de92e2279fdc07408787e22c45dbc0fefbc
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-06-18 00:57:01 +0530

    refactor:get the chats all from realtime db,no Firestore

--- client/src/components/chat/WidgetsChat.js
+++ client/src/components/chat/WidgetsChat.js
@@ -5,9 +5,11 @@ import React, { Suspense, useEffect, useState } from 'react'
 import './WidgetsChat.css'
 import { makeStyles} from '@material-ui/core/styles';
 import {Link} from 'react-router-dom';
-import {DataBase} from '../firebase'
+import {DataBase, realtime} from '../firebase'
 import {useStateValue} from '../../contexts/StateProvider';
 import Skeleton from '@material-ui/lab/Skeleton';
+import FlipMove from 'react-flip-move';
+
 const WidgetMember = React.lazy(() => import('./WidgetMember'))
 //==================================================Card Styles==============================================
 const useStyles = makeStyles((theme) => ({
@@ -52,25 +54,35 @@ function WidgetsChat() {
     const classes = useStyles();
     const [chats, setChats] = useState([])
     
-
-
     useEffect(() => {
         //if user is logged out it throws an error hence using try catch
         try {
-        
-                
-               DataBase.collection('users').doc(user.uid).collection('chats').onSnapshot((snapshot)=>(
-                    setChats(snapshot.docs.map((doc) => doc.data()))
-                ))
-                console.log(chats)
-                
-               
+        console.log(chats)
+
+          //get the username of the person we are chatting with 
+          realtime.ref(`/'chats'/${user.uid}`).orderByChild('lastchatAt').on('value', (snapshot) => {
+                //store chats of the particular user
+                const chatsArray = [] 
+            if (snapshot.exists()){
+                snapshot.forEach((child)=>{
+                  chatsArray.push(child.val())
+                })
+                console.log(snapshot.val())
+                console.log(chatsArray)
+
+                //.orderByChild works in ascending order
+                //we want in descending order hence reverse tha array
+                setChats(chatsArray.reverse())
+
+            // setChats(snapshot.val())
+            }
+        });
            
         }
         catch (error) {
             console.log(error.message+" coming from widgetsChat getting 'chats'collection ")
             }
-    }, [,user]);
+    }, [,user,chats.length]);
     console.log(chats)
 
 
@@ -81,12 +93,14 @@ function WidgetsChat() {
           {chats.length!==0 &&  
                 (chats.map((chat)=>(
                     <Suspense fallback={
-                        <div><Skeleton variant="text" />
+                        <div>
                         <Skeleton variant="circle" width={40} height={40} />
-                        <Skeleton variant="rect" width={210} height={118} /></div>} key={chat.chat_user_id}>
+                        <Skeleton variant="rect" width={210} height={50} /></div>} key={chat.key}>
+                      
                         <Link to={`/chats/${chat.chat_user_id}`} >
-                          <WidgetMember chatId={chat.chat_user_id} chat_username= {chat.chat_username} />
+                          <WidgetMember lastchatAt={chat.lastchatAt} chatId={chat.chat_user_id} chat_username= {chat.chat_username} />
                         </Link>
+
                         {console.log(chat.chat_user_id)}
                     </Suspense>
                 ))

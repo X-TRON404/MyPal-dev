3d1f9b2446a68b3d6153d17c3db53e4bac4989d2
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-06-18 00:38:02 +0530

    feat:added scroll to bottom when a new message is sent or received

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Chat.css'
 import {realtime} from '../firebase';
 import {useStateValue} from '../../contexts/StateProvider';
@@ -19,6 +19,10 @@ function Chat() {
     const [{user}, dispatch] = useStateValue();
     //get the slug from the url  (remeber that component using useParams should be inside <Router>)
     const {chatId} = useParams()
+    //
+    const scrollRef = useRef([])
+    //
+    const messageScrollRef = useRef(null)
 
 //======================================================GET Messages==========================================
 //everytime chat_user_id (which is basically the doc id inside the chat collection) changes pull the messages for that chat_user_id from the database
@@ -28,21 +32,21 @@ useEffect(() => {
     
             //here the chat_user_id (chatId) is taken from the user (user.uid) who posted that particular post to database
             const messagesArray = [];
-            realtime.ref(`/'messages'/${user.uid}/${chatId}`).on('value', (snapshot) => {
+            realtime.ref(`/'messages'/${user.uid}/${chatId}`).orderByChild('timestamp').on('value', (snapshot) => {
                 //get all the messages for that particular chatId
     
                 snapshot.forEach((child)=>{
                     messagesArray.push(child.val())
                 })
                 setMessages(messagesArray)
-                console.log("messages array "+ messagesArray)
+                // console.log("messages array "+ messagesArray)
                 
                 });
 
                 //get the username of the person we are chatting with 
                 realtime.ref(`/'chats'/${user.uid}/${chatId}`).on('value', (snapshot) => {
                 if (snapshot.exists()){
-                    console.log(snapshot.val())
+                    // console.log(snapshot.val())
                 setChatName(snapshot.val())
                 }
             });
@@ -55,32 +59,44 @@ useEffect(() => {
       
     }
 
+
 }
-
  //when chatId changes/messages are added/page is re-rendered  run this   
-}, [,chatId,messages])
+}, [,user,chatId,messages])
 
+//scroll to bottom when new messages are added.
+useEffect(() => {
+ 
+        scrollRef.current?.scrollIntoView({ behavior: "smooth" })
+        console.log("scrollHeight "+messageScrollRef.current.scrollHeight)
+        console.log("amount of scroll user has done "+messageScrollRef.current.scrollTop)
+    }
+    //using messages.length because firebase is constantly fetching messages from realtime db which changes messages state constantly
+    //but messages.length only changes when a new message is added
+,[messages.length])
 
 
 //=============================================================================================================
 
     return (                         
-             <div className="chat">
+             <div className="chat" >
                 <div className="chat__body">
                     <div className="chat__header" >
                         This is the beggining of your texx with  {chatName.chat_username}
                     </div>
-                        <div className="chat__messages">
+                        <div className="chat__messages" ref={messageScrollRef}>
                         {
                             messages.map((message)=>(
                                 <Suspense fallback={
-                                    <div><Skeleton variant="rect" width={210} height={118} /></div>} key={message.key}>
+                                    <div><Skeleton variant="rect" width={210} height={10} /></div>} key={message.key}>
                                         <Message message={message} key={message.key}/>
                                 </Suspense>
                                 ))
                             
                         }
                         </div>
+                <div ref={scrollRef}>
+                </div>
 
                 </div>
                 <div className="chat__sendMessage">

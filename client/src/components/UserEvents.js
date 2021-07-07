07ee36de8edec4616c8100f9a2dcd123bc50a658
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-07-07 12:29:16 +0530

    feat: Events of the user from <UserProfile>

---
+++ client/src/components/UserEvents.js
@@ -0,0 +1,47 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react'
+import { useStateValue } from '../contexts/StateProvider';
+import { DataBase } from './firebase';
+import './YourEvents.css'
+
+function UserEvents({palId}) {
+    //store user's events
+    const [events,setEvents] = useState([])
+    //convert date
+    const convertToDate = (date) => {
+        //convert to miliseconds
+        let k = date.seconds*1000
+        let dat = Date(k)
+        dat = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: "long" ,day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(k)
+        return dat
+    }
+    //====================================GET user created events=========================================
+    useEffect( () => {
+        //grab the events which belong to the logged in user from the db
+        DataBase.collection('events').where("user_id", "==", palId).get()
+        .then((querySnapshot) => {
+            setEvents(querySnapshot.docs.map(doc =>doc.data()))
+            console.log(events)
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+        
+    },[,palId]);
+    return (
+        <div className="yourevents">
+            {events.map(event=>(
+                    <Card className="yourevents__event" key={event.id}>
+                        <CardContent className="yourevents__eventContent">
+                            <Typography  component={'span'}>Event: {event.title}</Typography>
+                            <Typography  component={'span'}>Date: {convertToDate(event.dateTime)}</Typography>
+                            <Typography  component={'span'}>Venue: {event.venue}</Typography>
+                            <Typography  component={'span'}>Interested people: {event.interestedCount}</Typography>
+                        </CardContent>
+                    </Card>
+            ))}
+        </div>
+    )
+}
+
+export default UserEvents

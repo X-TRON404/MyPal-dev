5fef0242669f310d2ecc5ab05cd6474826a32f83
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-07-21 00:26:38 +0530

    feat: added <Home> component it will be developed later to have good graphics

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home" style={{color:'aliceblue'}}>
+            {/* MyPal. A step away from all your pals. */}
+        </div>
+    )
+}
+
+export default Home

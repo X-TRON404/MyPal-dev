843d251595b0bb81ef440917a3c92eb12e5e8091
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-10-28 12:32:23 +0530

    feat: completed issue #33 and issue #34

--- client/src/components/user/client/YourPosts.js
+++ client/src/components/user/client/YourPosts.js
@@ -1,37 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
 import { useStateValue } from '../../../contexts/StateProvider';
 import { DataBase } from '../../firebase';
 import './YourPosts.css'
-import { Link } from 'react-router-dom';
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: '#3F4347',
-  },
-  gridList:{
-    flex:1,
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: '#363A3E',
-    overflowY: 'scroll', 
-    width:'100%',
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-}));
+import YPost from './YPost'
 
 export default function YourPosts() {
-    const classes = useStyles();
-
     //get the user from the provider
     const [{user}, dispatch] = useStateValue();
     //posts array
@@ -54,24 +28,10 @@ export default function YourPosts() {
 
   return (
     <div className="yourPosts">
-      <GridList cellHeight={180} className={classes.gridList}>
             {posts.length!=0?(                            
-            posts.filter(post=>(post.post.imageUrl!='no-image')).map((post) => (
-            <GridListTile key={post.id}>
-                <img src={post.post.imageUrl} alt={post.post.caption} />
-                <Link to={`/yourposts/${post.id}`}>
-                <GridListTileBar
-                title={post.post.caption}
-                actionIcon={
-                    <IconButton aria-label={`info about ${post.post.caption}`} className={classes.icon}>
-                            <InfoIcon />
-                    </IconButton>
-                }
-                />
-                </Link>
-            </GridListTile>
+            posts.map((post) => (
+                  <YPost postId={post.post.id} username={post.post.username} user_id={post.post.user_id} caption={post.post.caption} imageUrl={post.post.imageUrl} timeInMillis={post.post?.timestamp?.seconds * 1000} likesCount={post.post.likesCount}/>
             ))):(<h4 style={{color:'aliceblue'}}>Empty. Nothing to see here</h4>)}
-      </GridList>
     </div>
   );
 }
